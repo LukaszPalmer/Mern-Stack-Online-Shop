27fe10ba495e6ab0005afb083141cea4e118eacc
LukaszPalmer <lukaszstanislawpalmer95@gmail.com>
2025-09-29 21:39:25 +0200

    added Tailwind.css + added Directories inside src

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
-    return <div>
-      
-    </div>;
+    return (
+        <div className="min-h-screen bg-slate-900 flex justify-center items-center">
+            <h1 className="text-5xl font-extrabold text-white underline decoration-sky-500">
+                Hallo Tailwind!
+            </h1>
+        </div>
+    );
 }
 
 export default App;
